48999654155a1867b99c497498fc154780761407
alhatmii <alhatmii.150@gmail.com>
2023-05-31 11:04:12 +0400

    First Commit

---
+++ src/test/java/codeLine/Task/Schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package codeLine.Task.Schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
