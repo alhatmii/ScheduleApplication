6d9320cd80038db67d1460401234afe750616467
alhatmii <alhatmii.150@gmail.com>
2023-05-31 11:11:25 +0400

    Added @Scheduling in Schedule Class

--- src/main/java/codeLine/Task/Schedule/Schedule.java
+++ src/main/java/codeLine/Task/Schedule/Schedule.java
@@ -1,4 +1,10 @@
 package codeLine.Task.Schedule;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.support.CronExpression;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Schedule {
+    @Scheduled 
 }
