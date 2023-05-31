f260b0b49ad7bd9a030ca49ccd32101137cf5f95
alhatmii <alhatmii.150@gmail.com>
2023-05-31 11:05:05 +0400

    Added @EnableScheduling

--- src/main/java/codeLine/Task/Schedule/ScheduleApplication.java
+++ src/main/java/codeLine/Task/Schedule/ScheduleApplication.java
@@ -2,8 +2,9 @@ package codeLine.Task.Schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class ScheduleApplication {
 	public static void main(String[] args) {
