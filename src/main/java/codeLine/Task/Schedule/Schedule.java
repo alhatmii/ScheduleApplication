20a7655b85ebdb786ae6efa858314ae868f72729
alhatmii <alhatmii.150@gmail.com>
2023-06-04 11:28:22 +0400

    Task 15

--- src/main/java/codeLine/Task/Schedule/Schedule.java
+++ src/main/java/codeLine/Task/Schedule/Schedule.java
@@ -5,13 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 */5 * * 1-5 *")
+    @Scheduled(cron = "0 30 23 13 * 5")
     public void schedule() {
         System.out.println("Drink Water!");
     }
-    @Scheduled(cron =  "0 */15 * * 6-7 *")
-    // Weekends
-    public void display(){
-        System.out.println("Drink water!!");
-    }
+
 }
